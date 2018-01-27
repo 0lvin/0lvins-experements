128f3f28624252e22cebb13a06ccc41451c895cf
Denis Pauk <pauk.denis@gmail.com>
2018-01-27 12:07:54 +0200

    unshare more

--- c/framebuffer/unshare.c
+++ c/framebuffer/unshare.c
@@ -8,18 +8,27 @@
 
 int main() {
 	int res = 0;
-	res = unshare(CLONE_NEWIPC | CLONE_NEWNS | CLONE_NEWNET | CLONE_NEWPID |
-		      CLONE_NEWUTS);
+	int pid = 0;
+	res = unshare(CLONE_NEWCGROUP | CLONE_NEWUSER | CLONE_NEWIPC |
+		      CLONE_NEWNS | CLONE_NEWNET | CLONE_NEWPID | CLONE_NEWUTS);
 
 	if (res < 0) {
 		perror("unshare");
 		return 0;
 	}
 
-	if(fork()) {
+	pid = fork();
+	if (pid < 0) {
+		perror("no fork?");
+		return 0;
+	}
+	if(pid) {
+		printf("I am parent for %d\n", pid);
 		wait(NULL);
 		return 0;
 	}
+
+	printf("I am child for %d\n", pid);
 
 	res = umount("/proc");
 	if (res < 0) {
@@ -29,7 +38,9 @@ int main() {
 	res = mount("proc", "/proc", "proc", 0, 0);
 	if (res < 0) {
 		perror("mount new proc");
+		return 0;
 	}
+	printf("New proc mounted");
 
 	res = execl("/bin/bash", "/bin/bash", NULL);
 	if (res < 0) {
