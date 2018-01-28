f4c423684cf577bc595fc3ab08c91dc9e731491c
Denis Pauk <pauk.denis@gmail.com>
2018-01-28 15:00:27 +0200

    premount dir's

--- c/framebuffer/unshare.c
+++ c/framebuffer/unshare.c
@@ -3,12 +3,55 @@
 #include <unistd.h>
 #include <sys/mount.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <stdio.h>
 
+
+int create_for_mount(const char *target, mode_t mode) {
+	int res = 0;
+
+	res =  mkdir(target, mode);
+	if (res < 0) {
+		perror("mkdir");
+		printf("can't create '%s' dir for mount\n", target);
+	}
+
+	printf("New dir for %s mount\n", target);
+	return 0;
+}
 
 int main() {
 	int res = 0;
 	int pid = 0;
+	char cwd[4048] = "\0";
+
+	// create and mount all dirs
+	if (create_for_mount("sys", S_IRUSR | S_IXUSR |
+				    S_IRGRP | S_IXGRP |
+				    S_IROTH | S_IXOTH) != 0) {
+		return 0;
+	}
+
+	if (create_for_mount("proc", S_IRUSR | S_IXUSR |
+				     S_IRGRP | S_IXGRP |
+				     S_IROTH | S_IXOTH) != 0) {
+		return 0;
+	}
+
+	if (create_for_mount("tmp", S_IRUSR | S_IWUSR | S_IXUSR |
+				    S_IRGRP | S_IWGRP | S_IXGRP |
+				    S_IROTH | S_IWOTH | S_IXOTH) != 0) {
+		return 0;
+	}
+
+	if (create_for_mount("dev", S_IRUSR | S_IWUSR | S_IXUSR |
+				    S_IRGRP | S_IXGRP |
+				    S_IROTH | S_IXOTH) != 0) {
+		return 0;
+	}
+
+	// go to separate namespace
 	res = unshare(CLONE_NEWCGROUP | CLONE_NEWUSER | CLONE_NEWIPC |
 		      CLONE_NEWNS | CLONE_NEWNET | CLONE_NEWPID | CLONE_NEWUTS);
 
@@ -17,32 +60,63 @@ int main() {
 		return 0;
 	}
 
+	// fork
 	pid = fork();
 	if (pid < 0) {
 		perror("no fork?");
 		return 0;
-	}
-	if(pid) {
+	} else if (pid) {
 		printf("I am parent for %d\n", pid);
 		wait(NULL);
+		umount("sys");
+		umount("proc");
+		umount("tmp");
+		umount("dev");
 		return 0;
 	}
 
 	printf("I am child for %d\n", pid);
 
-	res = umount("/proc");
-	if (res < 0) {
-		perror("unmount proc, not critical");
-	}
-
-	res = mount("proc", "/proc", "proc", 0, 0);
-	if (res < 0) {
-		perror("mount new proc");
+	// hide origin root
+	if (getcwd(cwd, sizeof(cwd)) == NULL) {
+		perror("current dir");
 		return 0;
 	}
-	printf("New proc mounted");
 
-	res = execl("/bin/bash", "/bin/bash", NULL);
+	printf("Curent dir is %s\n", cwd);
+
+	res = chroot(cwd);
+	if (res < 0) {
+		perror("chroot");
+		return 0;
+	}
+
+	// create and mount all dirs
+	res =  mount("sysfs", "sys", "sysfs", 0, 0);
+	if (res < 0) {
+		perror("mount sys");
+		return 0;
+	}
+
+	res =  mount("proc", "proc", "proc", 0, 0);
+	if (res < 0) {
+		perror("mount sys");
+		return 0;
+	}
+
+	res =  mount("tmpfs", "tmp", "tmpfs", 0, 0);
+	if (res < 0) {
+		perror("mount sys");
+		return 0;
+	}
+
+	res =  mount("tmpfs", "dev", "tmpfs", 0, 0);
+	if (res < 0) {
+		perror("mount dev, not critical");
+		return 0;
+	}
+
+	res = execl("/bin/sh", "/bin/sh", NULL);
 	if (res < 0) {
 		perror("execl");
 	}
