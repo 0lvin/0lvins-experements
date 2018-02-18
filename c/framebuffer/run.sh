3c07364d619362a2c151f1f2dc96bd9a04b3b25c
Denis Pauk <pauk.denis@gmail.com>
2018-02-18 12:09:07 +0200

    add repack

--- c/framebuffer/run.sh
+++ c/framebuffer/run.sh
@@ -10,4 +10,15 @@ wget -cv https://people.debian.org/~aurel32/qemu/armhf/vmlinuz-3.2.0-4-vexpress
 rm -rf check_image.qcow2
 qemu-img create -f qcow2 -o backing_file=debian_wheezy_armhf_standard.qcow2 check_image.qcow2
 
-qemu-system-arm -M vexpress-a9 -kernel vmlinuz-3.2.0-4-vexpress -initrd initrd.img-3.2.0-4-vexpress -drive if=sd,file=check_image.qcow2 -append "root=/dev/mmcblk0p2" -net nic,model=lan9118,netdev=net0 -netdev user,id=net0,hostfwd=tcp::2222-:22
+rm -rf initram
+mkdir initram
+cd initram
+
+gunzip -c ../initrd.img-3.2.0-4-vexpress | cpio -idmv
+
+find . | cpio -o -H newc | gzip > ../initrd.img-3.2.0-4-vexpress-new
+
+cd ..
+
+# qemu-system-arm -M vexpress-a9 -kernel vmlinuz-3.2.0-4-vexpress -initrd initrd.img-3.2.0-4-vexpress -drive if=sd,file=check_image.qcow2 -append "root=/dev/mmcblk0p2 console=ttyAMA0" -net nic,model=lan9118,netdev=net0 -netdev user,id=net0,hostfwd=tcp::2222-:22 -serial stdio
+qemu-system-arm -M vexpress-a9 -kernel vmlinuz-3.2.0-4-vexpress -initrd initrd.img-3.2.0-4-vexpress-new -drive if=sd,file=check_image.qcow2 -append "root=/dev/mmcblk0p2" -net nic,model=lan9118,netdev=net0 -netdev user,id=net0,hostfwd=tcp::2222-:22
