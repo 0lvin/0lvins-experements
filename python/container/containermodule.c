95603c453619f79d8d86d69791adc0c2a7190828
Denis Pauk <pauk.denis@gmail.com>
2018-03-28 23:54:00 +0300

    Add little more contants

--- python/container/containermodule.c
+++ python/container/containermodule.c
@@ -30,4 +30,15 @@ initcontainer(void)
 	m = Py_InitModule("container", ContainerMethods);
 	if (m == NULL)
 		return;
+
+	PyModule_AddIntConstant(m, "CLONE_FILES", CLONE_FILES);
+	PyModule_AddIntConstant(m, "CLONE_FS", CLONE_FS);
+	PyModule_AddIntConstant(m, "CLONE_NEWCGROUP", CLONE_NEWCGROUP);
+	PyModule_AddIntConstant(m, "CLONE_NEWIPC", CLONE_NEWIPC);
+	PyModule_AddIntConstant(m, "CLONE_NEWNET", CLONE_NEWNET);
+	PyModule_AddIntConstant(m, "CLONE_NEWNS", CLONE_NEWNS);
+	PyModule_AddIntConstant(m, "CLONE_NEWPID", CLONE_NEWPID);
+	PyModule_AddIntConstant(m, "CLONE_NEWUSER", CLONE_NEWUSER);
+	PyModule_AddIntConstant(m, "CLONE_NEWUTS", CLONE_NEWUTS);
+	PyModule_AddIntConstant(m, "CLONE_SYSVSEM", CLONE_SYSVSEM);
 }
