4b3fe15f5fe5bb63c81548a962d457539b5ffa09
Denis Pauk <pauk.denis@gmail.com>
2018-06-03 22:09:18 +0300

    Use presaved base

--- bash/builds/renegate-build.sh
+++ bash/builds/renegate-build.sh
@@ -7,54 +7,54 @@ fi;
 
 cd roc-rk3328-cc
 
-PWD=`pwd`
-echo "I am in ${PWD}"
+S_PWD=`pwd`
+echo "I am in ${S_PWD}"
 
 # U-Boot
-if [ ! -d "${PWD}/u-boot" ]; then
+if [ ! -d "${S_PWD}/u-boot" ]; then
     git clone -b release https://github.com/FireflyTeam/u-boot
 fi;
-cd "${PWD}/u-boot" && git pull
-cd "${PWD}"
+cd "${S_PWD}/u-boot" && git pull
+cd "${S_PWD}"
 
 # Kernel
-if [ ! -d "${PWD}/kernel" ]; then
+if [ ! -d "${S_PWD}/kernel" ]; then
     git clone -b release-4.4 https://github.com/FireflyTeam/kernel
 fi;
-cd "${PWD}/kernel" && git pull
-cd "${PWD}"
+cd "${S_PWD}/kernel" && git pull
+cd "${S_PWD}"
 
 # Build
-if [ ! -d "${PWD}/build" ]; then
+if [ ! -d "${S_PWD}/build" ]; then
     git clone -b debian https://github.com/FireflyTeam/build
 fi;
-cd "${PWD}/build" && git pull
-cd "${PWD}"
+cd "${S_PWD}/build" && git pull
+cd "${S_PWD}"
 
 # Rkbin
-if [ ! -d "${PWD}/rkbin" ]; then
+if [ ! -d "${S_PWD}/rkbin" ]; then
     git clone -b master https://github.com/FireflyTeam/rkbin
 fi;
-cd "${PWD}/rkbin" && git pull
-cd "${PWD}"
+cd "${S_PWD}/rkbin" && git pull
+cd "${S_PWD}"
 
 # Root fs
-if [ ! -d "${PWD}/rk-rootfs-build" ]; then
+if [ ! -d "${S_PWD}/rk-rootfs-build" ]; then
     git clone https://github.com/FireflyTeam/rk-rootfs-build.git
 fi;
-cd "${PWD}/rk-rootfs-build" && git pull
-cd "${PWD}"
+cd "${S_PWD}/rk-rootfs-build" && git pull
+cd "${S_PWD}"
 
 # build
 ./build/board_configs.sh roc-rk3328-cc
 ./build/mk-uboot.sh roc-rk3328-cc
 ./build/mk-kernel.sh roc-rk3328-cc
 
-sudo dpkg -i ${PWD}/ubuntu-build-service/packages/*
+sudo dpkg -i ${S_PWD}/ubuntu-build-service/packages/*
 sudo apt-get install -f -y
 
-cd "${PWD}/rk-rootfs-build"
+cd "${S_PWD}/rk-rootfs-build"
 VERSION=stretch TARGET=desktop ARCH=armhf ./mk-base-debian.sh
-cd "${PWD}"
+cd "${S_PWD}"
 
 #### http://wiki.t-firefly.com/ROC-RK3328-CC/linux_build_rootfilesystem.html
