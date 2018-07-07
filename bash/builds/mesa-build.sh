a7ae899ce39b9fab1cba6c84b35f5414af44aabf
Denis Pauk <pauk.denis@gmail.com>
2018-07-07 14:50:41 +0300

    astc tests

--- bash/builds/mesa-build.sh
+++ bash/builds/mesa-build.sh
@@ -6,10 +6,10 @@ fi;
 
 cd mesa
 git pull
-autoreconf -vfi
-make distclean
+#autoreconf -vfi
+#make distclean
 
-CXX='ccache g++' CC='ccache gcc' ./configure --enable-texture-float --with-llvm-prefix=/usr/lib/llvm-5.0/ --enable-gallium-osmesa --with-sha1=libgcrypt --enable-llvm --with-gallium-drivers=swrast,swr --with-dri-drivers=swrast
+CXX='ccache g++' CC='ccache gcc' ./configure --enable-texture-float --with-llvm-prefix=/usr/lib/llvm-6.0/ --enable-gallium-osmesa --with-sha1=libgcrypt --enable-llvm --with-gallium-drivers=swrast --with-dri-drivers=swrast --enable-lmsensors
 CXX='ccache g++' CC='ccache gcc' make
 
 cd ..
@@ -22,17 +22,14 @@ cp -rv mesa/lib/gallium/* lib/
 LIBGL_DRIVERS_PATH=`pwd`/lib glxinfo
 echo `pwd`/lib
 
-if [ ! -d piglit ]; then
-    git clone git://anongit.freedesktop.org/piglit
-fi;
+#if [ ! -d piglit ]; then
+#    git clone git://anongit.freedesktop.org/piglit
+#fi;
+#cd piglit
+#git pull
+#CXX='ccache g++' CC='ccache gcc' cmake .
+#make
 
-cd piglit
-
-git pull
-
-CXX='ccache g++' CC='ccache gcc' cmake .
-
-make
-
-LIBGL_ALWAYS_SOFTWARE=true GALLIUM_DRIVER=swr LIBGL_DRIVERS_PATH=`pwd`/lib piglit/bin/bptc-modes
-LIBGL_ALWAYS_SOFTWARE=true GALLIUM_DRIVER=swr LIBGL_DRIVERS_PATH=`pwd`/lib piglit/bin/bptc-float-modes
+#LIBGL_ALWAYS_SOFTWARE=true GALLIUM_DRIVER=softpipe LIBGL_DRIVERS_PATH=`pwd`/lib PIGLIT_SOURCE_DIR=`pwd`/piglit piglit/bin/bptc-modes
+#LIBGL_ALWAYS_SOFTWARE=true GALLIUM_DRIVER=softpipe LIBGL_DRIVERS_PATH=`pwd`/lib PIGLIT_SOURCE_DIR=`pwd`/piglit piglit/bin/bptc-float-modes
+LIBGL_ALWAYS_SOFTWARE=true GALLIUM_DRIVER=softpipe LIBGL_DRIVERS_PATH=`pwd`/lib PIGLIT_SOURCE_DIR=`pwd`/piglit piglit/bin/khr_compressed_astc-miptree_gl
