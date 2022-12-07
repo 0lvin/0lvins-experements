30528e0f592806282afc04e27da47dad507e31bd
Denis Pauk <pauk.denis@gmail.com>
2022-12-07 22:49:26 +0200

    change partuuid

--- bash/chromebook/create.sh
+++ bash/chromebook/create.sh
@@ -10,12 +10,12 @@ echo "console=ttyS2,115200n8 earlyprintk=ttyS2,115200n8 console=tty1 init=/sbin/
 vbutil_kernel --pack /tmp/vmlinux.kpart --version 1 --vmlinuz /tmp/vmlinux.uimg --arch aarch64 --keyblock /usr/share/vboot/devkeys/kernel.keyblock --signprivate /usr/share/vboot/devkeys/kernel_data_key.vbprivk --config /tmp/cmdline --bootloader /tmp/bootloader.bin
 
 # sudo dd if=/dev/mmcblk0p1 of=`uname -r`.img
-sudo dd bs=4096 if=/dev/disk/by-partuuid/fd6d3a79-215a-8845-8ff5-d0d7ea912df0 of=`uname -r`.img
+sudo dd bs=4096 if=/dev/disk/by-partuuid/2d0afc5d-f4b8-df43-b45a-18b34d662753 of=`uname -r`.img
 
 # sudo dd if=/dev/zero of=/dev/mmcblk0p1
-sudo dd bs=4096 if=/dev/zero of=/dev/disk/by-partuuid/fd6d3a79-215a-8845-8ff5-d0d7ea912df0
+sudo dd bs=4096 if=/dev/zero of=/dev/disk/by-partuuid/2d0afc5d-f4b8-df43-b45a-18b34d662753
 
 # sudo dd if=/tmp/vmlinux.kpart of=/dev/mmcblk0p1
-sudo dd bs=4096 if=/tmp/vmlinux.kpart of=/dev/disk/by-partuuid/fd6d3a79-215a-8845-8ff5-d0d7ea912df0
+sudo dd bs=4096 if=/tmp/vmlinux.kpart of=/dev/disk/by-partuuid/2d0afc5d-f4b8-df43-b45a-18b34d662753
 
 sleep 30 && sync
